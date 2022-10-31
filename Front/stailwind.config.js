4f11a355b3d910c80d939ea28d2c93b8f060a751
musubi42 <raphael.dev@protonmail.com>
2022-10-31 11:23:36 +0100

    Add postcss-cli

---
+++ Front/stailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx", "./node_modules/flowbite/**/*.js"],
+    theme: {
+      extend: {
+        colors: {
+        'primary': '#11203A',
+        'secondary': '#FFCA0C'
+        },
+        padding: {
+          '1/2': '50%',
+          'full': '100%'
+        },
+        backgroundImage: {
+          'logoWhite': "url(Assets/Icons/logoWhite.svg)",
+          'formContact': "url(Assets/BGFormContact.webp)",
+          'AlfonZoAcademyMockup': "url(Assets/AlfonZoAcademyMockup.webp)",
+          'RoswellMockup': "url(Assets/RoswellMockup.webp)",
+        }
+      },
+    },
+    plugins: [
+      require('flowbite/plugin')
+    ],
+  }
