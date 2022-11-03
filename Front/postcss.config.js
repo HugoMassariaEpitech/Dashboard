a0b2259cc04e32189ea6593c91d4945287331eb4
musubi42 <raphael.dev@protonmail.com>
2022-11-03 14:47:59 +0100

    Add cssnano for prod

--- Front/postcss.config.js
+++ Front/postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    // TODO: Trouver le moyen de ne pas utiliser cssnano en dev avec process.env.NODE_ENV
+    // require('cssnano')({
+    //   preset: 'default',
+    // }),
   ]
 }
