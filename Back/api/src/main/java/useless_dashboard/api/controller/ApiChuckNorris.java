3dac0724c3e7f99dcd6913284a14cf14713adbc4
musubi42 <raphael.dev@protonmail.com>
2022-11-09 18:04:01 +0100

    Get Joke with param or not

--- Back/api/src/main/java/useless_dashboard/api/controller/ApiChuckNorris.java
+++ Back/api/src/main/java/useless_dashboard/api/controller/ApiChuckNorris.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,9 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiChuckNorris {
     @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping
-    public ResponseEntity<?> getJoke() throws Exception {
+    public @ResponseBody ResponseEntity<?> getJoke(@RequestParam(required = false) String category) throws Exception {
+        // If no category is specified, return a random joke
+        String url = category == null ? "https://api.chucknorris.io/jokes/random" : "https://api.chucknorris.io/jokes/random?category=" + category;
         HttpRequest request = HttpRequest.newBuilder()
-		.uri(URI.create("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random"))
+		.uri(URI.create(url))
 		.header("accept", "application/json")
 		.header("X-RapidAPI-Key", "05ac93870amsh40fcb7719a01364p190b5ajsn263d10c2c29f")
 		.header("X-RapidAPI-Host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com")
